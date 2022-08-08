2cbfcd78c6ec55962c26b3306749cba7749c94bf
Rajeshwari Subramanian <110542520+RajeshwariSubramanian@users.noreply.github.com>
2022-08-08 14:41:52 +0530

    Update Calculator.java

--- day2/Calculator.java
+++ day2/Calculator.java
@@ -1,45 +1,48 @@
+
 package week1.day2;
 
 public class Calculator {
+	public static void main(String[] args) {
+
+		Mycalculator cal1 = new Mycalculator();
+		System.out.println("Sum of Numbers:" + cal1.addNumbers);
+		System.out.println("Difference of Numbers:" + cal1.subNumbers);
+		System.out.println("Product :" + cal1.mulNumbers);
+		System.out.println("Quotient :" + cal1.divNumber);
+
+	}
 
 	public int addThreeNumbers(int num1, int num2, int num3) {
-		int number = num1 + num2 + num3;
-		return number;
+		int sum = num1 + num2 + num3;
+		return sum;
 	}
 
 	public int subTwoNumbers(int num1, int num2) {
-		int number = num1 - num2;
-		return number;
+		int diff = num1 - num2;
+		return diff;
 	}
 
-	public double multiplyNumbers(double num1, double num2) {
-		double number = num1 * num2;
-		return number;
+	public double multiplyNumbers(int num1, int num2) {
+		double prod = num1 * num2;
+		return prod;
 	}
 
-	public float divideNumbers(float num1, float num2) {
-		float number = num1 / num2;
-		return number;
+	public float divideNumbers(int num1, int num2) {
+		float quotient = num1 / num2;
+		return quotient;
 	}
+}
 
-	
-	
-		public static void main(String[] args) {
-			
-			Calculator cal = new Calculator();
-			int addNumbers = cal.addThreeNumbers(5, 6, 2);
-			System.out.println("Sum of Numbers:" + addNumbers);
-			int subNumbers = cal.subTwoNumbers(65, 34);
-			System.out.println("Difference of Numbers:" + subNumbers);
-			double mulNumbers = cal.multiplyNumbers(5, 6);
-			System.out.println("Product :" + mulNumbers);
-			float divNumber = cal.divideNumbers(65, 5);
-			System.out.println("Quotient :" + divNumber);
+class Mycalculator {
+	Calculator cal = new Calculator();
+	int addNumbers = cal.addThreeNumbers(5, 6, 2);
+	int subNumbers = cal.subTwoNumbers(65, 34);
+	double mulNumbers = cal.multiplyNumbers(5, 6);
+	float divNumber = cal.divideNumbers(65, 5);
 
-		}
+}
+
 		
-		
-		}
 
 
 	
