4aea5549b31a1aef6b192d2eb1f09be236b962d0
Rajeshwari Subramanian <110542520+RajeshwariSubramanian@users.noreply.github.com>
2022-08-18 14:24:03 +0530

    Update DeleteLead.java

--- week2/day2/DeleteLead.java
+++ week2/day2/DeleteLead.java
@@ -9,12 +9,13 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DeleteLead {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		// Launch URL
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 
 		// MAXIMIZE THE WINDOW
 		driver.manage().window().maximize();
@@ -40,34 +41,39 @@ public class DeleteLead {
 		driver.findElement(By.xpath("//span[text()='Phone']")).click();        
 		driver.findElement(By.xpath("//input[@name='phoneNumber']")).sendKeys("8939439034");
 		
+		
 		// Click Find leads button
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
 
-	  	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		Thread.sleep(2000);
 
 		// Capture lead ID of First Resulting lead
 		String elementLeadID = driver.findElement(By.xpath("//div/div[1]/table/tbody/tr[1]/td[1]/div/a")).getText();
 		System.out.println(elementLeadID);
 		
+		Thread.sleep(2000);
 		// Click First Resulting lead
 		driver.findElement(By.xpath("//div/div[1]/table/tbody/tr[1]/td[1]/div/a")).click();
 		
+		Thread.sleep(2000);
 		// Click Delete
 		driver.findElement(By.xpath("//a[text()='Delete']")).click();
 		
-        //Click Find leads
+		Thread.sleep(2000);
+                 //Click Find leads
 		driver.findElement(By.xpath("//a[text()='Find Leads']")).click();
 		
+		Thread.sleep(2000);
 		// Enter captured lead ID
 		driver.findElement(By.xpath("//input[@name='id']")).sendKeys(elementLeadID);
 		
 		// Click find leads button
 		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
-		
+		Thread.sleep(2000);
 		// Verify message "No records to display" in the Lead List
 		String elementCapturedID = driver.findElement(By.xpath("//div[1]/div/div/div[2]//div[contains(text(),'No records')]")).getText();
-		
-        //Close the browser 
+		Thread.sleep(2000);
+               //Close the browser 
 		if (elementCapturedID.contains("No records to display")) {
 			driver.close();
 		}else
