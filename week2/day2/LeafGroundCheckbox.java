f17ee57264f2e71b8c131d080a52f351890b652e
Rajeshwari Subramanian <110542520+RajeshwariSubramanian@users.noreply.github.com>
2022-08-19 17:01:41 +0530

    Update LeafGroundCheckbox.java

--- week2/day2/LeafGroundCheckbox.java
+++ week2/day2/LeafGroundCheckbox.java
@@ -16,73 +16,71 @@ public class LeafGroundCheckbox {
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://www.leafground.com/checkbox.xhtml");
-
+		driver.manage().window().maximize();
 		
 		//Basic Checkbox
 		
-		 WebElement elementBasicCheckBox=driver.findElement(By.id("j_idt87:j_idt89_input"));
+		 WebElement elementBasicCheckBox=driver.findElement(By.xpath("//input[@aria-label='Basic']"));		
 		 System.out.println("Is Basic checkbox selected:" +elementBasicCheckBox.isSelected());
-		 
-		 Thread.sleep(2000);
+		  	 Thread.sleep(2000);
+
 	      
 		 //Notification 
 		 for(int i=0;i<2;i++)
 		 {
-		 WebElement elementNotification=driver.findElement(By.xpath("//div[@id='j_idt87:j_idt91']/div[2]"));
+		 WebElement elementNotification=driver.findElement(By.xpath("//div[1]/div[2]/div/div/div/div[2]"));
 		 elementNotification.click();
 		 Thread.sleep(2000);
-		 WebElement elementNotificationMessage=driver.findElement(By.xpath("//div[@id='j_idt87:msg_container']/div/div/div[2]/span"));
+		 WebElement elementNotificationMessage=driver.findElement(By.xpath("//div[5]/div/div/div[2]/span"));
 		 System.out.println("The Notification Checkbox is:" +elementNotificationMessage.getText());
 		 }
 		   
-		  	 
+		 Thread.sleep(2000);	 
 		 //Choose your favorite language(s)
-		 WebElement elementFavLanguage=driver.findElement(By.xpath("//table[@id='j_idt87:basic']/tbody/tr/td[1]/div"));
+		 WebElement elementFavLanguage=driver.findElement(By.xpath("//table[contains(@class,'selectmanycheckbox')]/tbody//td[1]/div[1]/div[2]"));
 		 elementFavLanguage.click();
-		 WebElement elementFavLanguage1=driver.findElement(By.xpath("//table[@id='j_idt87:basic']/tbody/tr/td[2]/div"));
+		 WebElement elementFavLanguage1=driver.findElement(By.xpath("//table[contains(@class,'selectmanycheckbox')]/tbody//td[2]/div[1]/div[2]"));
 		 elementFavLanguage1.click();
 		 Thread.sleep(2000);
 		 
 		 //Tri State Checkbox
 		 for(int i=0;i<3;i++)
 		 {
-		 WebElement elementTriState=driver.findElement(By.xpath("//div[@id='j_idt87:ajaxTriState']/div[2]"));
+		 WebElement elementTriState=driver.findElement(By.xpath("//form/div/div[1]/div[4]/div//div[2]"));
 		 elementTriState.click();
 		 Thread.sleep(2000);
-		 WebElement elementTriState1=driver.findElement(By.xpath("//div[@id='j_idt87:id_container']/div/div/div[2]/span"));
-		 WebElement elementTriState1Text=driver.findElement(By.xpath("//div[@id='j_idt87:id_container']/div/div/div[2]/p"));
+		 WebElement elementTriState1=driver.findElement(By.xpath("//div[6]/div/div/div[2]/span"));
+		 WebElement elementTriState1Text=driver.findElement(By.xpath("//div/div/div[2]/p"));
 		 System.out.println(elementTriState1.getText() + ":"+ elementTriState1Text.getText());
 		 }
-		 Thread.sleep(2000);
+		 Thread.sleep(3000);
 		 
 		 //Toggle Switch
 		 for(int i=0;i<2;i++){
-		 WebElement elementToggleSwitchCheck=driver.findElement(By.xpath("//div[@id='j_idt87:j_idt100']/div[2]"));
+		 WebElement elementToggleSwitchCheck=driver.findElement(By.xpath("//form/div/div[2]/div[1]/div/div[2]"));
 		 elementToggleSwitchCheck.click();
 		 Thread.sleep(2000);
-		 WebElement elementToggleMessage1=driver.findElement(By.xpath("//div[@id='j_idt87:msg_container']/div/div/div[2]/span"));
+		 WebElement elementToggleMessage1=driver.findElement(By.xpath("//div[5]/div/div/div[2]/span"));
 		 System.out.println("The Toggle Switch is:" +elementToggleMessage1.getText());
 		 }
 		 Thread.sleep(2000);
 			
 		 //Verify if check box is disabled
-		 WebElement elementVerifyCheckBox=driver.findElement(By.xpath("//div[@id='j_idt87:j_idt102']"));
+		 WebElement elementVerifyCheckBox=driver.findElement(By.xpath("//form/div/div[2]/div[2]/div/div/div"));
 		 boolean elementCheckBoxDisabled=elementVerifyCheckBox.isSelected();
 		 System.out.println("Is CheckBox Selected :" +elementCheckBoxDisabled);
 		 
 		//Select Multiple Checkboxes
-		 WebElement elementClickCheckBox=driver.findElement(By.xpath("//div[@id='j_idt87:multiple']/div[3]/span"));
+		 WebElement elementClickCheckBox=driver.findElement(By.xpath("//form/div/div[2]/div[3]/div/div[3]"));
 		 elementClickCheckBox.click();
-		 WebElement elementClickSelect=driver.findElement(By.xpath("//div[@id='j_idt87:multiple_panel']/div[2]/ul"));
-		 elementClickSelect.click();
-		 
 		 Thread.sleep(2000);
-		 WebElement elementClickCheckbox1=driver.findElement(By.xpath("//div[@id='j_idt87:multiple_panel']/div[1]/div[1]/div[2]"));
+		 WebElement elementClickCheckbox1=driver.findElement(By.xpath("//div[7]/div[1]/div[1]/div[2]"));
 		 elementClickCheckbox1.click();
-		 WebElement elementClickClose=driver.findElement(By.xpath("//div[@id='j_idt87:multiple_panel']/div[1]/a"));
+		 Thread.sleep(2000);
+		 WebElement elementClickClose=driver.findElement(By.xpath("//div[7]/div[1]/a"));
 		 elementClickClose.click();
 		 Thread.sleep(2000);
-		 
+		
 		 driver.close();
 			
 		 }
