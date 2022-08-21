13a508cbec304d0290217eb615fd3d7d2148382c
Rajeshwari Subramanian <110542520+RajeshwariSubramanian@users.noreply.github.com>
2022-08-21 17:50:29 +0530

    Add files via upload

---
+++ Week 3/day 1/bank/AxisBank.java
@@ -0,0 +1,18 @@
+package bank;
+
+public class AxisBank extends BankInfo {
+	public void	deposit()
+	{
+		System.out.println("Deposit in Account:12000");
+	}
+	
+
+	public static void main(String[] args) {
+		AxisBank axis=new AxisBank();
+		axis.saving();
+		axis.fixed();
+		axis.deposit();
+
+	}
+
+}
---
+++ Week 3/day 1/bank/BankInfo.java
@@ -0,0 +1,21 @@
+package bank;
+
+public class BankInfo {
+
+public void	saving()
+{
+	System.out.println("Savings in Account:20000");
+}
+public void	fixed()
+{
+	System.out.println("Fixed Deposit:100000");
+}
+public void	deposit()
+{
+	System.out.println("Deposit in Account:5000");
+}
+	
+	
+	
+	
+}
